489ee988711489779e7abdb8aee6ac637231fa29
tomm-rodion <autovolna@gmail.com>
2023-03-22 22:57:51 +0200

    Update HW_6

--- js/task-06.js
+++ js/task-06.js
@@ -1,16 +1,40 @@
 "use strict";
-const textInput = document.querySelector("#validation-input");
-console.log(textInput);
-const inputValueDefaultLength = Number(textInput.dataset.length);
-console.log(inputValueDefaultLength);
+// const textInput = document.querySelector("#validation-input");
+// console.log(textInput);
+// const inputValueDefaultLength = Number(textInput.dataset.length);
+// console.log(inputValueDefaultLength);
 
-textInput.addEventListener("blur", onCallbackInput);
-function onCallbackInput() {
-  if (inputValueDefaultLength === textInput.value.length) {
-    textInput.classList.remove("invalid");
-    return textInput.classList.add("valid");
-  } else {
-    textInput.classList.remove("valid");
-    return textInput.classList.add("invalid");
+// textInput.addEventListener("blur", onCallbackInput);
+// function onCallbackInput() {
+//   if (inputValueDefaultLength === textInput.value.length) {
+//     textInput.classList.remove("invalid");
+//     return textInput.classList.add("valid");
+//   } else {
+//     textInput.classList.remove("valid");
+//     return textInput.classList.add("invalid");
+//   }
+// }
+
+//#2
+
+const elInput = document.querySelector('input[data-length="6"]');
+const classInput = document.querySelector("#validation-input");
+
+function onChange(e) {
+  if (e.currentTarget.value.length === 0) {
+    return;
+  } else if (e.currentTarget.value.length !== Number(elInput.dataset.length)) {
+    classInput.classList.add("invalid");
+  } else if (e.currentTarget.value.length === Number(elInput.dataset.length)) {
+    classInput.classList.add("valid");
   }
 }
+
+function onCleerInput(e) {
+  classInput.classList.remove("valid");
+  classInput.classList.remove("invalid");
+  e.currentTarget.value = "";
+}
+
+elInput.addEventListener("blur", onChange);
+elInput.addEventListener("focus", onCleerInput);
