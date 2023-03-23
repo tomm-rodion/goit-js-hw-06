261ab7aa898e1af1a15db691d366df840041178c
tomm-rodion <autovolna@gmail.com>
2023-03-23 12:20:57 +0200

    Update Hw6_refactoring

--- js/task-07.js
+++ js/task-07.js
@@ -1,10 +1,19 @@
-const input = document.querySelector("#font-size-control");
+const elInput = document.querySelector("#font-size-control");
 const spanText = document.querySelector("#text");
+//#1
 
-input.addEventListener("input", hendlerInput);
-let value = 0;
+// elInput.addEventListener("input", hendlerInput);
+// let value = 0;
 
-function hendlerInput(evt) {
-  value = evt.currentTarget.value;
-  spanText.style.fontSize = value + "px";
+// function hendlerInput(evt) {
+//   value = evt.currentTarget.value;
+//   spanText.style.fontSize = value + "px";
+// }
+
+//#2 refactoring code
+
+function onSizeText(e) {
+  spanText.style.fontSize = `${e.target.value}px`;
 }
+
+elInput.addEventListener("input", onSizeText);
